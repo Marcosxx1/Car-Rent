521e12066005e1ab1350a769cfa7be9cf7dd16af
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-13 18:30:31 -0400

    feat: create a categoryService, with dependency inversion

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,26 @@
+import { CategoriesRepository } from "../repository/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+/* Quando este error aparecer
+Property 'X' does not exist on type 'Y'.ts(2339)
+
+Verificar se existe o atributo no construtor  (private categoriesRepository: CategoriesRepository) por exemplo
+ */
+class CreateCategoryService {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: CategoriesRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+export { CreateCategoryService };
