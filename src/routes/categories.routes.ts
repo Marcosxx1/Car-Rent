2fd2be8726c98ee454cf1136b03b220db087c4d3
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-13 15:31:33 -0400

    feat: add get route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 
-import { CategoriesRepository } from "../repository/ategoriesRepository";
+import { CategoriesRepository } from "../repository/CategoriesRepository";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -11,6 +11,11 @@ categoriesRoutes.post("/", (req: Request, res: Response) => {
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req: Request, res: Response) => {
+  const all = categoriesRepository.list();
+  return res.json(all);
 });
 
 export { categoriesRoutes };
