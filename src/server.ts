8266941ee8ddcabf97bd3500fb759026fd9b7c4c
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-13 12:31:00 -0400

    chore: use /categories on app.use route

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
