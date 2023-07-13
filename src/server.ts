c7d17317c3e33ab7c204697f4ff08d93b1486358
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-13 12:18:50 -0400

    feat: import categoriesRoutes for use in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
 import express from "express";
 
+// Como não é um export default, a importação precisa estar entre {}
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-  return res.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
