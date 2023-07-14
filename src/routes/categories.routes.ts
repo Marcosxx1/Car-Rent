983b986e06887a3c6a6b0c93d0520874ab122e02
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-14 12:25:01 -0400

    refactor: Use createCategoryController's handle function to deal with req, res

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,13 @@
 import { Router, Request, Response } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repository/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-  return res.status(201).send();
+  return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req: Request, res: Response) => {
