326297999ba32f457a41e8adb0f73a222e266d2c
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-14 10:24:31 -0400

    feat: Create SpecificationRepository

---
+++ src/modules/cars/repository/SpecificationRepository.ts
@@ -0,0 +1,34 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from "./ISpecificationRepository";
+
+class SpecificaionRepository implements ISpecificationRepository {
+  private specifications: Specification[] = [];
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  list(): Specification[] {
+    return this.specifications;
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name,
+    );
+    return specification;
+  }
+}
+
+export { SpecificaionRepository };
