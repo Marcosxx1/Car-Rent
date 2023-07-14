f49347d6cf978ede59e3b16e27559edd5cabd230
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-14 10:26:32 -0400

    feat(server): add /specifications endpoint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 // Como não é um export default, a importação precisa estar entre {}
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationRoutes } from "./routes/specifications.route";
 
 const app = express();
 
@@ -10,6 +11,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
