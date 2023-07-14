c7c9d8f1a2efa66006edf6e79bd7a4263a640dec
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-14 10:22:48 -0400

    feat: Create interface for SpecificationRepositry

---
+++ src/modules/cars/repository/ISpecificationRepository.ts
@@ -0,0 +1,11 @@
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationRepository {
+  findByName(name: string): ICreateSpecificationDTO;
+  create({ name, description }: ICreateSpecificationDTO): void;
+}
+
+export { ISpecificationRepository, ICreateSpecificationDTO };
