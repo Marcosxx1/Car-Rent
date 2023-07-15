27e6279a6834646ffa058a2d3cdeee914a4adee4
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-15 18:09:29 -0400

    feat: Use multer to upload files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) =>
   createCategoryController.handle(req, res),
@@ -12,6 +17,12 @@ categoriesRoutes.post("/", (req, res) =>
 // eslint-disable-next-line prettier/prettier
 categoriesRoutes.get("/", (req: Request, res: Response) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+  console.log(file);
+  return res.send();
 });
 
 export { categoriesRoutes };
