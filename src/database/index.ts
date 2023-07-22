7397bf62146435ce8ef7b2e5955beca6921f4b04
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-22 14:49:29 -0400

    fix: Use correct path

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,16 +2,18 @@ import "reflect-metadata";
 
 import { DataSource } from "typeorm";
 
+import { Category } from "../modules/cars/model/Category";
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "banco",
+  host: "localhost",
   port: 5432,
   username: "postgres",
   password: "postgres",
   database: "rentx",
   synchronize: true,
   logging: false,
-  entities: [],
-  migrations: [],
+  entities: [Category],
+  migrations: ["src/database/migrations/*.ts"], // Correct the path to migrations
   subscribers: [],
 });
