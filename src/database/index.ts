0bf19e837991d762f92b7838ce3313cdf58b9473
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-22 08:07:01 -0400

    feat: Add connection to docker database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,17 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
 
-import { Category } from "../modules/cars/model/Category";
-import { Specification } from "../modules/cars/model/Specification";
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "banco",
   port: 5432,
   username: "postgres",
   password: "postgres",
-  database: "test",
+  database: "rentx",
   synchronize: true,
   logging: false,
-  entities: [Category, Specification],
+  entities: [],
   migrations: [],
   subscribers: [],
 });
