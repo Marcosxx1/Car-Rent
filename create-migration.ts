83f81920f68505586820b9877e356fd485ff4038
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-24 07:45:44 -0400

    feat: Create scprit to create migration

---
+++ create-migration.ts
@@ -0,0 +1,19 @@
+import { execSync } from "child_process";
+
+const migrationDirectory = "./src/database/migrations";
+
+const [, , migrationName] = process.argv;
+
+if (!migrationName) {
+  console.error("Uso: ts-node create-migration.ts <nome_migration>");
+  process.exit(1);
+}
+
+const command = `yarn typeorm migration:create ${migrationDirectory}/${migrationName}`;
+
+try {
+  execSync(command);
+  console.log(`Migration "${migrationName}" criada com sucesso!`);
+} catch (error) {
+  console.error("Ocorreu um erro ao criar a migration:", error.message);
+}
