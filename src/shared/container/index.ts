5997be9b7f834a9830be57bbd3c3b231e1b4d583
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-26 08:42:45 -0400

    Add singleton fo SpecificationRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,16 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repository/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repository/implementations/CategoriesRepository";
+import { SpecificationRepository } from "../../modules/cars/repository/implementations/SpecificationRepository";
+import { ISpecificationRepository } from "../../modules/cars/repository/ISpecificationRepository";
 
 /* ICategoriesRepository */
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository,
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepository,
 );
