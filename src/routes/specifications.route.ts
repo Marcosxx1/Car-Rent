b43cd48efe76ff41e4a6cac8d4fb9b272a3f2cd8
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-26 13:43:52 -0400

    refactor: Use createSpecificationController instance instead of its import

--- src/routes/specifications.route.ts
+++ src/routes/specifications.route.ts
@@ -1,16 +1,11 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/CreateSpecification/CreateSpecificationController";
-import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
 specificationRoutes.post("/", createSpecificationController.handle);
-
-specificationRoutes.get("/", (req: Request, res: Response) => {
-  return listCategoriesController.handle(req, res);
-});
 
 export { specificationRoutes };
