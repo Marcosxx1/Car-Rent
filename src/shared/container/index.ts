f93a1746ccb8b453ea53faeba3e543554fb83424
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-26 07:22:41 -0400

    refactor: Use Tsyringe to dependency injection instead of index inside each useCase

---
+++ src/shared/container/index.ts
@@ -0,0 +1,10 @@
+import { container } from "tsyringe";
+
+import { ICategoriesRepository } from "../../modules/cars/repository/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/repository/implementations/CategoriesRepository";
+
+/* ICategoriesRepository */
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository,
+);
