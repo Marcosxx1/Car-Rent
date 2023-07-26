f9bd4e833a5e45e269793a2a335218aedb460880
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-26 13:41:27 -0400

    refactor: Use container to dependency injection instead of passing it through the constructor

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private importCtegoryUseCase: ImportCategoryUseCase) { }
-
-  handle(req: Request, res: Response): Response {
+  async handle(req: Request, res: Response): Promise<Response> {
     const { file } = req;
-    this.importCtegoryUseCase.execute(file);
+
+    const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
+
+    await importCategoryUseCase.execute(file);
     return res.send();
   }
 }
