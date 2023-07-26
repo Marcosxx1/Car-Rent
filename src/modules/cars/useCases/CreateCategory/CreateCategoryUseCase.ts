f2b3adc0cb83e91e42447fd20c51cf80211965af
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-26 07:27:43 -0400

    refactor: Use Tsyringe inject to inject CategoriesRepository. Make the class injectable

--- src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.ts
@@ -1,17 +1,20 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repository/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
-/* Quando este error aparecer
-Property 'X' does not exist on type 'Y'.ts(2339)
 
-Verificar se existe o atributo no construtor  (private categoriesRepository: CategoriesRepository) por exemplo
- */
+@injectable()
 class CreateCategoryUseCase {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private categoriesRepository: ICategoriesRepository) { }
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository,
+  ) {
+    console.log("");
+  }
 
   async execute({ name, description }: IRequest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(
