b7771dd84b14017f4752a3dd9f219151680dfd12
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-26 08:54:18 -0400

    refactor: Add import reflect-metadata. Import container and use it for dependency injection

--- src/modules/cars/useCases/CreateSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/CreateSpecification/CreateSpecificationController.ts
@@ -1,15 +1,18 @@
+import "reflect-metadata";
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { CreateSpecificationUseCase } from "./CreateSpecifiationUseCase";
 
 class CreateSpecificationController {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) { }
-
-  handle(req: Request, res: Response): Response {
+  async handle(req: Request, res: Response): Promise<Response> {
     const { name, description } = req.body;
 
-    this.createSpecificationUseCase.execute({ name, description });
+    const createSpecificationUseCase = container.resolve(
+      CreateSpecificationUseCase,
+    );
+
+    await createSpecificationUseCase.execute({ name, description });
 
     return res.status(201).send();
   }
