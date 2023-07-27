93c5e99217d33f0b8c78c424c0d1e181f4a781f1
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-27 15:47:33 -0400

    feat: Add findByEmail method

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -1,9 +1,11 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 interface IUserRepository {
   /* Posso desestruturar os dados ou somente pegar como
   esta aqui em baixo data: do tipo da interface */
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUserRepository };
