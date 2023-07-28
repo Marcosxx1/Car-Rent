c3d2d687e61628f383c0ee82f3429f7c0eedf4b2
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-28 13:12:28 -0400

    feat: Move create user use case and controller to its onw folder

---
+++ src/modules/accounts/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUserCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, email, driver_license, password } = req.body;
+
+    const createUserUserCase = container.resolve(CreateUserUserCase);
+
+    await createUserUserCase.execute({
+      name,
+      email,
+      driver_license,
+      password,
+    });
+    return res.status(201).send();
+  }
+}
+
+export { CreateUserController };
