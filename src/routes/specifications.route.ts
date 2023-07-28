5a31b704f11fa59df0ca47b87c003e23c702c153
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-28 14:46:40 -0400

    feat: Added ensureAuthenticaded middleware to post specifications

--- src/routes/specifications.route.ts
+++ src/routes/specifications.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/CreateSpecification/CreateSpecificationController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationRoutes.use(ensureAuthenticated);
 specificationRoutes.post("/", createSpecificationController.handle);
 
 export { specificationRoutes };
