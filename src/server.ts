b5ddc0af6898638e0dae36aaf6a7a1e1a31a1a50
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-28 18:40:16 -0400

    feat: Add error handling middleware to handle AppError and internal server errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import swaggerFile from "../swagger.json";
@@ -7,6 +8,7 @@ import { router } from "./routes";
 import "./database";
 
 import "./shared/container";
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
@@ -14,9 +16,22 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use(router);
+app.use(router); 
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
