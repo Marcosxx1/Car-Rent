1a938d92d81a63b97c5b6993c6de30a3f213efae
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-07-31 14:40:32 -0400

    feat: Add findById method

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -2,6 +2,7 @@ import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { User } from "../entities/User";
 
 interface IUserRepository {
+  findById(id: string): Promise<User>;
   /* Posso desestruturar os dados ou somente pegar como
   esta aqui em baixo data: do tipo da interface */
   create(data: ICreateUserDTO): Promise<void>;
