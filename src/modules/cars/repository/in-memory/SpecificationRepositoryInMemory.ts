6c50e9b02d81328e4d2b70154b2df1ddbe021c20
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-08-15 14:11:08 -0400

    feat: In memory category and specification repositories that implements its interfaces

---
+++ src/modules/cars/repository/in-memory/SpecificationRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { Category } from "../../entities/Category";
+import { Specification } from "../../entities/Specification";
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+
+
+
+class SpecificationRepositoryInMemory implements ICategoriesRepository {
+
+  specification: Specification[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const specification = this.specification.find(specification => specification.name === name);
+    return Promise.resolve(specification);
+  }
+  async list(): Promise<Category[]> {
+    const all = this.specification;
+    return Promise.resolve(all);
+
+  }
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const specification = new Specification();
+    Object.assign(specification, {
+      name,
+      description
+    })
+
+    this.specification.push(specification);
+  }
+}
+
+export { SpecificationRepositoryInMemory }
